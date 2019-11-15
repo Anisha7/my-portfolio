1fbbdb38af4ad94c28083a2777c143cf2020ca5a
Anisha Jain <anishajain@outlook.com>
2019-11-15 02:42:51 -0800

    contact links added

--- src/components/Home/components/Contact/index.js
+++ src/components/Home/components/Contact/index.js
@@ -2,21 +2,34 @@ import React, { Component } from 'react';
 import './styles.css';
 
 class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showEmail: false,
+        }
+    }
     render() {
+        let emailText = "Email me."
+        if (this.state.showEmail) {
+            emailText = "anishaj037@gmail.com"
+        }
+
         return (
                 <div className="contact content-margin">
                     <h1>Your turn.</h1>
                     <p>Get in touch! How can I help you?</p>
                     <div className="table">
-                        <div className="col one">
-                            <p>Email me.</p>
-                        </div>
+                        <a href="mailto:anishaj037@gmail.com" className="col one" 
+                            onMouseOver={ () => this.setState({ showEmail: true }) }
+                            onMouseOut={ () => this.setState({ showEmail: false }) }>
+                            <p>{ emailText }</p>
+                        </a>
                         <div class="hidden-div">
                             <div class="triangle"></div> 
                         </div>
-                        <div className="col two">
+                        <a href="https://www.linkedin.com/in/annysha/" className="col two" target="_blank">
                             <p>Linkedin connect.</p>
-                        </div>
+                        </a>
                         <div class="hidden-div-two">
                             <div class="triangle-two"></div> 
                         </div>
