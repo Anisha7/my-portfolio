f1f7df02c7e9e9a5c0b2b07e16302e94ce894e98
Anisha Jain <anishajain@outlook.com>
2019-11-15 00:11:14 -0800

    Basic banner layout implemented

---
+++ src/components/Home/components/Banner/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './styles.css';
+import image from '../../../../images/banner.svg';
+
+class Banner extends Component {
+    render() {
+        return (
+                <div className="banner">
+                    <div className="slogan">
+                        <h1>I am a <span className="highlight">passionate</span></h1>
+                        <h1>frontend engineer</h1>
+                    </div>
+                    <img src={ image } alt="Illustration of creative work space" />
+                </div>
+            )
+    }
+}
+
+export default Banner;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './styles.css'
 import Wrapper from '../Wrapper';
+import Banner from './components/Banner';
 
 class Home extends Component {
 
     render() {
         return (
                 <Wrapper>
+                    <Banner />
                     Hello
                 </Wrapper>
             )
